0f7d1e97fe98add7a203f709ffc08dc6510b4c8c
煌士 櫻庭 <kira.sakura@outlook.jp>
2026-04-17 12:39:39 +0900

    ブルースクリーンの作成

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,59 @@
 #include <Windows.h>
+#include <KamataEngine.h>
+#include "GameScene.h"
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) 
+{
+	//DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	//エンジンの初期化
+	KamataEngine::Initialize(L"LE3D_10_サクラバ_キラト_CG4");
+
+	//ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+	//ゲームシーンの初期化
+	gameScene->Initialize();
+
+
+
+	//メインループ
+	while (true) 
+	{
+		//エンジンの更新
+		if (KamataEngine::Update()) 
+		{
+			break;
+		}
+
+		//ゲームシーンの更新
+		gameScene->Update();
+
+
+		//描画開始
+		dxCommon->PreDraw();
+
+
+		//ゲームシーン描画
+		gameScene->Draw();
+
+
+		//描画終了
+		dxCommon->PostDraw();
+
+	}
+
+	//ゲームシーンの解放
+	delete gameScene;
+	//nullptrの代入
+	gameScene = nullptr;
+
+	//エンジンの終了処理
+	KamataEngine::Finalize();
+
+
 	return 0;
 }
